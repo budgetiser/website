8a0b189bca7fd3bfbf01b6f4e675c904e2d742cc
aschulte201 <a.schulte201@gmail.com>
2023-06-24 11:56:51 +0200

    initial website

---
+++ src/app/app.module.ts
@@ -0,0 +1,44 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; 
+
+// Material
+import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatSidenavModule } from '@angular/material/sidenav';
+
+// Routing
+import { RouterModule, Routes } from '@angular/router'
+
+// Custom
+import { AppComponent } from './app.component';
+import { PrivacyComponent } from './privacy/privacy.component';
+import { DisclaimerComponent } from './disclaimer/disclaimer.component';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+  { path: 'home', component:  HomeComponent},
+  { path: 'privacy', component:  PrivacyComponent},
+  { path: 'disclaimer', component:  DisclaimerComponent},
+  { path: '', redirectTo: '/home', pathMatch: 'full' }
+]
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PrivacyComponent,
+    DisclaimerComponent,
+    HomeComponent
+  ],
+  imports: [
+    RouterModule.forRoot(routes),
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatTooltipModule,
+    MatSidenavModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
